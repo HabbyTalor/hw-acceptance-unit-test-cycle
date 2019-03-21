f8f80b3d9bc48309614b6fef2f2c17dce8acebd2
Kamila Michel <g00340498@gmit.ie>
2019-03-21 15:09:18 +0000

    Added test for oddness method

--- rottenpotatoes/spec/movie_controller_spec.rb
+++ rottenpotatoes/spec/movie_controller_spec.rb
@@ -2,29 +2,32 @@ require 'rails_helper'
 
 describe MoviesController, type: 'controller' do
     let(:movies) { ['star wars', 'raiders'] }    
-
-    context '#similar_movies' do
+    
+    context '#search_directors' do
         describe 'movie has a director' do
              # integration test (route spec)
+             expect(Movie).to receive(:where).with(director:'dummy director')
             it 'responds to the search_directors route' do
-              get :similar_movies, { id: 1 }
+              get :search_directors, { id: 1 }
             end
         end 
-
+        
         describe 'search similar movies' do
           it 'queries the Movie model about similar movies' do
             # YOUR TEST CODE HERE
             # Remove the pending and fail lines to execute your test code
+            
             pending "Implement this test!"
-            fail
+            #fail
           end
-
+          
           it 'assigns similar movies to the template' do
             # YOUR TEST CODE HERE
             # Remove the pending and fail lines to execute your test code
+            
             pending "Implement this test!"
-            fail
+            #fail
           end
         end
     end    
-end 
+end
